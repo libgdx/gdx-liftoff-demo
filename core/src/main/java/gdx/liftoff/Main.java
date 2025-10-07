6b095a3e6369ecef1bd03e50d42237a16e2fc388
tommyettinger <tommy.ettinger@gmail.com>
2025-10-07 04:45:31 +0000

    Automatic deployment: gdx-liftoff 1.13.5.3.

--- core/src/main/java/gdx/liftoff/Main.java
+++ core/src/main/java/gdx/liftoff/Main.java
@@ -1,8 +1,6 @@
 package gdx.liftoff;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
